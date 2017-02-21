0ea348484eaced910c7976dbdfaedeed35dd7365
Wujek <mjwojcik94@gmail.com>
2017-02-21 14:53:06 +0100

    jedno z jQ jedno z JS

--- js/script.js
+++ js/script.js
@@ -1,49 +1,70 @@
 'use strict';
 
 $(function() {
+   
+//Echo
    console.log("Document is ready Sir.");
+   
+// Kolorowanie trzeciego paragrafu   
+   $("#list").children('p:nth-child(3)').css('color','red');
+   
+//Event Listener
+   $('input[value=DOWN]').click(down);
+   $('input[value=UP]').click(up);
+
 });
 
+/*******************************************************************/
 
 function down() {
    
    var first = $("#list").children("p:first-child");
       
    var rest = $("#list p");
-
+   
    rest.appendTo("#list");
    first.appendTo("#list");
+   
+   // z wykorzystaniem jQuery
+      // nie udało mi się zrobić analogicznego w drugą stronę
+         // wiec napisałem to w JS
+
 }
 
 function up() {
    
-   var last = $("#list").children("p:last-child");
+   var paragraphs = document.getElementsByTagName('p');
+   var count = paragraphs.length;
+   var last = paragraphs[count-1];
+   var temp = []; // tymczasowa tablica
    
-   var rest = $("#list p");
+   for(var i=0; i<count; i++){
+      (i===0)? temp[0]=last : temp[i]=paragraphs[i-1];
+   } // dla pierwszego elementu przyjmuje wartosc ostatniego paragrafu
    
-   last.appendTo("#list");
-   rest.appendTo("#list");
+   for(var j=0; j<count; j++){
+      paragraphs[j].outerHTML = temp[j].outerHTML;
+   }
    
+   console.log("Nie wiem jakim cudem to działa.");
+   
+   //z wykorzystaniem JScript
 }
-   
-$('input[value=DOWN]').click(down);
-$('input[value=UP]').click(up);
-   
 
-/*var allP = $('p');
-var listLength = allP.children().prevObject.length;
+/*******************************************************************/
 
-var temp1 = allP.children().prevObject[0];
+// nieudana wersja z jQ
 
-console.log();*/
-
-//var list = $( "p" ).detach();
-//
-//var listLength = list.children().length;
-//var first = list.children()[0];
-//var last = list.children()[listLength-1];
+//function up() {
 //   
-//var newList = list;
-
-//$("#list:nth-child").remove();
+//   var last = $("#list").children("p:last-child");
+//   
+//   var rest = $("#list p");
+//   
+//   $("#list p").detach("p");
+//   last.appendTo("#list p");
+//   rest.appendTo("#list p");
+//   
+//}
+   
 
